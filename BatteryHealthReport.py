02dd8c89f2883137270de7efc943c712f9711eac
WissamAlAtat <Wissamworkstation@gmail.com>
2024-10-18 22:09:36 +0300

    Finalized Script

--- BatteryHealthReport.py
+++ BatteryHealthReport.py
@@ -7,3 +7,12 @@ def getDesktopPath():
     user=getpass.getuser()
     desktopPath=f"C:\\Users\\{user}\\Desktop\\battery-report.html"
     return desktopPath
+
+def run_powershell_as_admin(command):
+    powershell_command = f'powershell.exe -Command "Start-Process PowerShell -ArgumentList \'{command}\' -Verb RunAs"'
+    subprocess.run(powershell_command, shell=True)
+
+
+output_path = getDesktopPath()
+command = f'powercfg /batteryreport /output "{output_path}"'
+run_powershell_as_admin(command)
